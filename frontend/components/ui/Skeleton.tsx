4951b0ee4a6724cf29424de5fc6a1bd82bd7264d
edenbd1 <eden.baudin.invest@gmail.com>
2026-04-01 16:07:43 +0200

    chore: remove frontend folder

--- frontend/components/ui/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-interface SkeletonProps {
-  className?: string;
-}
-
-export function Skeleton({ className = '' }: SkeletonProps) {
-  return (
-    <div className={`animate-pulse bg-surface-2 rounded ${className}`} />
-  );
-}
--- frontend/lib/abis/index.ts
+++
@@ -1,7 +0,0 @@
-export { XYieldVaultABI } from "./XYieldVault";
-export { AutocallEngineABI } from "./AutocallEngine";
-export { ProtocolStatsABI } from "./ProtocolStats";
-export { SablierStreamABI } from "./SablierStream";
-export { EpochManagerABI } from "./EpochManager";
-export { ERC20ABI } from "./ERC20";
-export { NoteTokenABI } from "./NoteToken";
